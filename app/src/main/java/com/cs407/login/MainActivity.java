319f6923f6285a40a8e0599b9c061afb06b5b811
postinspector <efamiliant@gmail.com>
2023-10-30 08:32:48 -0500

    Initial commit - login

---
+++ app/src/main/java/com/cs407/login/MainActivity.java
@@ -0,0 +1,40 @@
+package com.cs407.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUsername, editTextPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Implement your login logic here
+                // Validate credentials, authenticate, and proceed accordingly
+            }
+        });
+    }
+
+    public void openSignUpScreen(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/cs407/login/SignUpActivity.java
@@ -0,0 +1,42 @@
+package com.cs407.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText editTextNewUsername, editTextEmail, editTextNewPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        editTextNewUsername = findViewById(R.id.editTextNewUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextNewPassword = findViewById(R.id.editTextNewPassword);
+
+        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newUsername = editTextNewUsername.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String newPassword = editTextNewPassword.getText().toString();
+
+                // Implement your sign-up logic here
+                // Store new user information or perform desired actions
+            }
+        });
+    }
+
+    public void openLoginScreen(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
