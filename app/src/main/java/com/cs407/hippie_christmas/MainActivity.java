e3f900fbf80803070a61f350438ec4335453be50
eliza <efamiliant@gmail.com>
2023-11-29 08:41:41 -0600

    Login + save user creds works!

--- app/src/main/java/com/cs407/hippie_christmas/MainActivity.java
+++ app/src/main/java/com/cs407/hippie_christmas/MainActivity.java
@@ -7,13 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
     EditText editTextUsername, editTextPassword;
+    DatabaseHelper databaseHelper; // Ensure you have a DatabaseHelper class for SQLite operations
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
+        databaseHelper = new DatabaseHelper(this); // Initialize the database helper
 
         Button buttonLogin = findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
@@ -29,13 +30,29 @@ public class MainActivity extends AppCompatActivity {
                 String username = editTextUsername.getText().toString();
                 String password = editTextPassword.getText().toString();
 
-                // Implement your login logic here
-                // Validate credentials, authenticate, and proceed accordingly
+                // Check if user exists in the database
+                if (databaseHelper.checkUser(username, password)) {
+                    // Login successful, navigate to HomePageActivity
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent homeIntent = new Intent(MainActivity.this, home_page.class);
+                    startActivity(homeIntent);
+                } else {
+                    // User not found, show error message
+                    Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpScreen();
             }
         });
     }
 
-    public void openSignUpScreen(View view) {
+    private void openSignUpScreen() {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/cs407/hippie_christmas/SignUpActivity.java
+++ app/src/main/java/com/cs407/hippie_christmas/SignUpActivity.java
@@ -5,22 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.cs407.hippie_christmas.MainActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText editTextNewUsername, editTextEmail, editTextNewPassword;
+    DatabaseHelper databaseHelper; // Assuming you have a DatabaseHelper class
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        // Initialize the EditTexts and DatabaseHelper
         editTextNewUsername = findViewById(R.id.editTextNewUsername);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextNewPassword = findViewById(R.id.editTextNewPassword);
+        databaseHelper = new DatabaseHelper(this);
 
         Button buttonSignUp = findViewById(R.id.buttonSignUp);
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
@@ -30,13 +33,21 @@ public class SignUpActivity extends AppCompatActivity {
                 String email = editTextEmail.getText().toString();
                 String newPassword = editTextNewPassword.getText().toString();
 
-                // Implement your sign-up logic here
-                // Store new user information or perform desired actions
+                // Add input validation here if needed
+
+                // Save the user information to the database
+                databaseHelper.addUser(newUsername, email, newPassword);
+
+                // Show a confirmation message
+                Toast.makeText(SignUpActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+
+                // Navigate back to the MainActivity
+                openLoginScreen();
             }
         });
     }
 
-    public void openLoginScreen(View view) {
+    public void openLoginScreen() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
